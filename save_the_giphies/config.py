0c123358a79f5c520c5c50c26e07230fbd526dd4
Enrique Matta-Rodriguez <enrique@matta.dev>
2020-01-08 20:17:12 -0600

    1 - Save the Giphies Retriever
    - Added dataclass config file
    - Basic logic for retrieving giphies

---
+++ save_the_giphies/config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import Optional
+import os
+
+
+@dataclass
+class Config:
+    giphy_schema: str = "https"
+    giphy_host: str = "api.giphy.com"
+    giphy_version: Optional[str] = os.getenv("GIPHY_API_VERSION", "v1")
+    giphy_search_endpoint: str = "gifs/search"
+    giphy_api_key: Optional[str] = os.getenv("GIPHY_API_KEY")
+
+
+config = Config()
